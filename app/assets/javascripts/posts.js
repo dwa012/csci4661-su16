0e68b723ae78bf30056f371c911159cde6f808ef
Daniel Ray Ward <drward3@uno.edu>
2016-06-23 20:13:10 -0500

    using a remote comments form and some more css interactions

---
+++ app/assets/javascripts/posts.js
@@ -0,0 +1,7 @@
+$(document).on('ready page:load', function() {
+    //$('.post-list .post-body').hover(function() {
+    //    $(this).removeClass('clipped');
+    //}, function() {
+    //    $(this).addClass('clipped');
+    //});
+});
